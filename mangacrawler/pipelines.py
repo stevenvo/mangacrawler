cfac93bf96aeadb19a35bfc1d3cea7157f2a0d76
Steven Vo <findme@stevenvo.info>
2015-05-30 00:45:04 -0700

    support one page manga

--- mangacrawler/pipelines.py
+++ mangacrawler/pipelines.py
@@ -12,29 +12,35 @@ import re
 
 class GocThuGianImagesPipeline(ImagesPipeline):
 
-  CONVERTED_ORIGINAL = re.compile('^full/[0-9,a-f]+.jpg$')
+   CONVERTED_ORIGINAL = re.compile('^full/[0-9,a-f]+.jpg$')
 
-  # name information coming from the spider, in each item
-  # add this information to Requests() for individual images downloads
-  # through "meta" dict
-  def get_media_requests(self, item, info):
-    print "get_media_requests"
-    return [Request(x, meta={'chapter_id': item["chapter_id"], 'part_id': item["part_id"], 'book_name': item["book_name"]})
+   # name information coming from the spider, in each item
+   # add this information to Requests() for individual images downloads
+   # through "meta" dict
+   def get_media_requests(self, item, info):
+      print "get_media_requests"
+      return [Request(x, meta={'chapter_id': item["chapter_id"], 'part_id': item["part_id"], 'book_name': item["book_name"]})
       for x in item.get('image_urls', [])]
 
-  # this is where the image is extracted from the HTTP response
-  def get_images(self, response, request, info):
-    print "get_images"
-    for key, image, buf, in super(GocThuGianImagesPipeline, self).get_images(response, request, info):
-      if self.CONVERTED_ORIGINAL.match(key):
-        key = self.change_filename(key, response)
-      yield key, image, buf
 
-  def change_filename(self, key, response):
-    org_filename = response.url.split('/')[-1]
-    new_filename = '{0}-Tap{1}-Chapter{2}-Image{3}'.format(response.meta['book_name'], response.meta['part_id'], response.meta['chapter_id'], org_filename)
-    return '%s' % (new_filename)
-    # return "full/%s.jpg" % response.meta['chapter'][0]
+   # this is where the image is extracted from the HTTP response
+   def get_images(self, response, request, info):
+      print "get_images"
+      for key, image, buf, in super(GocThuGianImagesPipeline, self).get_images(response, request, info):
+         if self.CONVERTED_ORIGINAL.match(key):
+            key = self.change_filename(key, response)
+            yield key, image, buf
+
+
+   def change_filename(self, key, response):
+      org_filename = response.url.split('/')[-1]
+      if re.findall('(\d*).jpg', org_filename)[0] != '':
+         image_id = int(re.findall('(\d*).jpg', org_filename)[0])
+      else:
+         image_id = int(re.findall('Trang(\d*)_TSMini.jpg', org_filename)[0])
+
+      new_filename = '{0}-Tap{1}-Chapter{2}-Image{3:06}.jpg'.format(response.meta['book_name'], response.meta['part_id'], response.meta['chapter_id'], image_id)
+      return '%s' % (new_filename)
 
 
 class MangacrawlerPipeline(object):
--- mangacrawler/spiders/gocthugian.py
+++ mangacrawler/spiders/gocthugian.py
@@ -13,12 +13,21 @@ class GocthugianSpider(scrapy.Spider):
    def __init__(self, *args, **kwargs): 
       super(GocthugianSpider, self).__init__(*args, **kwargs) 
       self.book_name = kwargs.get('book_name')
-      self.start_part_id = int(kwargs.get('start'))
-      self.stop_part_id = int(kwargs.get('stop'))
-      self.start_urls = [kwargs.get('start_url')]
+      try:
+         self.start_part_id = int(kwargs.get('start')) 
+         self.stop_part_id = int(kwargs.get('stop'))
+      except TypeError:
+         self.start_part_id = 1
+         self.stop_part_id = 100
+      self.start_urls = [kwargs.get('url')]
 
    def parse(self, response):
       parts = response.xpath('//div[@class="VIOI"]/a') #Tập 
+      
+      if len(parts) == 0: #truyen nay ko co tap, chi co' chuong
+         self.no_part = True
+         yield scrapy.Request(response.url, callback=self.parsePart) #pass processing to part page
+         
       for part in parts:
          part_id = int(part.xpath('./text()').re('T.*?p (\d*)')[0])
          if part_id >= self.start_part_id and part_id <= self.stop_part_id:
@@ -26,8 +35,8 @@ class GocthugianSpider(scrapy.Spider):
             yield scrapy.Request(part_link, callback=self.parsePart)
 
    def parsePart(self, response):
-      chapters = scrapy.Selector(response).xpath('//li[@class="ChI"]/a')
-      for chapter in chapters:
+      chapters = response.xpath('//li[@class="ChI"]/a')
+      for chapter in chapters:         
          chapter_link = urlparse.urljoin(response.url, chapter.xpath('./@href').extract()[0])
          yield scrapy.Request(chapter_link, callback=self.parseChapterPage)
 
@@ -35,6 +44,11 @@ class GocthugianSpider(scrapy.Spider):
       item = MangacrawlerItem()
       item['book_name'] = self.book_name
       page_title = response.xpath('//div[@class="FPH"]/h1/text()').extract()[0]
-      (item['part_id'], item['chapter_id']) = re.findall("T.*?p (\d*) - Chapter (\d*)", page_title)[0]
+      part_id_arr = re.findall("T.*?p (\d*) -", page_title)
+      if len(part_id_arr) == 0:
+         item['part_id'] = 1
+      else:
+         item['part_id'] = part_id_arr[0]
+      item['chapter_id'] = re.findall("Chapter (\d*)", page_title)[0]
       item['image_urls'] = response.xpath('//div[@class="TTCD"]/img/@src').extract()
       return item
